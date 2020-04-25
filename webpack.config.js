c09a3340da2d43fc8cdf70287f48179d3711d2d2
Ryan <moncrief.ryan@gmail.com>
2020-04-24 22:39:43 -0500

    Development Demo Done

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
              app: './src/index.js',
              print: './src/print.js',
            },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     plugins: [
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
@@ -16,6 +21,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
     
 };
