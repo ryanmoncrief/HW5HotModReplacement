87679f31a589be7aa3be2e892ec071a26bbe7cdd
Ryan <moncrief.ryan@gmail.com>
2020-04-24 22:49:25 -0500

    Hot module replacement Demo done

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: {
              app: './src/index.js',
-             print: './src/print.js',
            },
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
+        hot: true,
     },
+    module: {
+        rules: [
+            {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+            },
+        ],
+           },
     plugins: [
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
